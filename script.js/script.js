9dd5d67ed88129f6d6c1e805d70cd7079173720f
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2023-09-22 18:10:38 +0400

    first commit

---
+++ script.js/script.js
@@ -0,0 +1,48 @@
+// SWIPER
+// var swiper = new Swiper('#js-carousel', {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   speed: 2000,
+//   loopedSlides: 10,
+//   loop: true,
+//   //allowTouchMove: false, // можно ещё отключить свайп
+//   autoplay: {
+//     delay: 0,
+//     disableOnInteraction: false // или сделать так, чтобы восстанавливался autoplay после взаимодействия
+//   }
+// });
+
+// ==================================
+// new CircleProgress('.progress', {
+// 	max: 100,
+// 	value: 60,
+// 	textFormat: 'percent',
+// });
+// ==================================
+
+// SLICK-CORUSEL
+// $('.center').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
